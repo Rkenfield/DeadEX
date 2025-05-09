472ea1ce75b34474cca03db9d6bae4cdda67c0f5
KyleN <kyle@boldergames.com>
2025-05-09 09:24:35 -0600

    Empty 5.5 project

---
+++ Source/DeadEX/DeadEX.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "DeadEX.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, DeadEX, "DeadEX" );
